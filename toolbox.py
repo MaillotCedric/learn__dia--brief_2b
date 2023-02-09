05d8dbe89dcb9784e457f1505e264609e6c9b1b8
Cédric MAILLOT <cedricmaillot38@gmail.com>
2023-02-09 13:15:41 +0400

    Update classifr

--- toolbox.py
+++ toolbox.py
@@ -93,5 +93,23 @@ def graph(metric, training_metrics):
 
     return plt
 
-def save_model():
-    pass
+from PIL import Image
+def get_image(image_path, shape):
+    image = Image.open(image_path).resize(shape)
+
+    return image
+
+def get_labels(datasets_path):
+    labels = tf.keras.preprocessing.image_dataset_from_directory(datasets_path).class_names
+
+    return labels
+
+import numpy as np
+def predict_image(model, image_path, shape, datasets_path="data/images"):
+    image = get_image(image_path, shape)
+    np_image = np.asarray(image)
+    labels = get_labels(datasets_path)
+    predictions = model.predict(np.array([np_image]))
+    label_index = np.argmax(predictions)
+
+    return labels[label_index]
